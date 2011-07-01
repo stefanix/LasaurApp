2bccaa42ce8731fc2e4c2ceed347a3589e45e3c0
Stefan Hechenberger <stefan@nortd.com>
2011-07-01 15:13:33 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,132 @@
+
+import time
+import os.path
+import wsgiref.simple_server
+from bottle import *
+import serial_manager, serial
+
+
+#ARDUINO_PORT = '/dev/tty.usbmodem411'
+ARDUINO_PORT = '/dev/tty.usbmodem621'
+BAUDRATE = 9600
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+def run_with_callback(host='127.0.0.1', port=8080, timeout=0.01):
+    """ Start a wsgiref server instance with control over the main loop.
+        This is a function that I derived from the bottle.py run()
+    """
+    handler = default_app()
+    server = wsgiref.simple_server.make_server(host, port, handler)
+    server.timeout = timeout
+    print "Bottle server starting up ..."
+    print "Listening on http://%s:%d/" % (host, port)
+    print "Use Ctrl-C to quit."
+    print
+    while 1:
+        try:
+            serial_manager.send_queue_as_ready()
+            server.handle_request()
+        except KeyboardInterrupt:
+            break
+    print "\nShutting down..."
+    serial_manager.close()
+
+
+
+@route('/hello')
+def hello_handler():
+    return "Hello World!!"
+
+@route('/connect')
+def connect_handler():
+    global arduino_port, baudrate
+    try:
+        serial_manager.connect(ARDUINO_PORT, BAUDRATE)
+        ret = "Serial connected to %s:%d." % (ARDUINO_PORT, BAUDRATE)  + '<br>'
+        time.sleep(0.5) # allow some time to receive a prompt/welcome
+        return serial_manager.get_responses('<br>')
+    except serial.SerialException:
+        print "Failed to connect to serial."    
+        return ""    
+
+@route('/longtest')
+def longtest_handler():
+    fp = open("longtest.ngc")
+    for line in fp:
+        serial_manager.queue_for_sending(line)
+    return "Longtest queued."
+    
+
+
+@route('/css/:path#.+#')
+def static_css_handler(path):
+    return static_file(path, root=os.path.join(current_dir, 'css'))
+    
+@route('/js/:path#.+#')
+def static_css_handler(path):
+    return static_file(path, root=os.path.join(current_dir, 'js'))
+    
+@route('/img/:path#.+#')
+def static_css_handler(path):
+    return static_file(path, root=os.path.join(current_dir, 'img'))
+
+@route('/')
+@route('/index.html')
+@route('/app.html')
+def default_handler():
+    return static_file('app.html', root=current_dir)
+    
+
+@route('/serial/:connect')
+def serial_handler(connect):
+    if connect == '1' and not serial_manager.is_connected():
+        try:
+            global arduino_port, baudrate
+            serial_manager.connect(ARDUINO_PORT, BAUDRATE)
+            ret = "Serial connected to %s:%d." % (ARDUINO_PORT, BAUDRATE)  + '<br>'
+            time.sleep(0.5) # allow some time to receive a prompt/welcome
+            return serial_manager.get_responses('<br>')
+        except serial.SerialException:
+            print "Failed to connect to serial."    
+            return ""          
+    elif connect == '0' and serial_manager.is_connected():
+        if serial_manager.close(): return "1"
+        else: return ""  
+    elif connect == "2":
+        if serial_manager.is_connected(): return "1"
+        else: return ""
+    else:
+        print 'got neither: ' + connect            
+        return ""
+        
+
+@route('/gcode/:gcode_line')
+def gcode_handler(gcode_line):
+    if serial_manager.is_connected():    
+        print gcode_line
+        serial_manager.queue_for_sending(gcode_line + '\n')
+        return "Queued for sending."
+    else:
+        return ""
+
+@route('/gcode', method='POST')
+def gcode_handler_submit():
+    gcode_program = request.forms.get('gcode_program')
+    if gcode_program and serial_manager.is_connected():
+        print gcode_program
+        lines = gcode_program.split('\n')
+        for line in lines:
+            serial_manager.queue_for_sending(line + '\n')
+        return "Queued for sending."
+    else:
+        return ""
+
+@route('/queue_pct_done')
+def queue_pct_done_handler():
+    return serial_manager.get_queue_percentage_done()
+
+
+debug(True)
+run_with_callback(host='localhost', port=8080)
---
+++ serial_manager.py
@@ -0,0 +1,120 @@
+
+from collections import deque
+import time
+import serial
+
+serial_port = None
+
+gcode_queue = deque()  # could use a list, but deque is faster
+GRBL_BUFFER_MAX = 3
+grbl_buffer_current = 0
+gcode_queue_count = 0
+total_items_queued_in_batch = 0  # used for calculating percentage done
+
+responses = deque()
+RESPONSES_MAX = 50
+responses_current = 0
+
+
+
+def append_response(line):
+    # ring buffer-style responses queuing
+    global responses, responses_current, RESPONSES_MAX
+    if responses_current >= RESPONSES_MAX:
+        responses.popleft()
+        responses_current -= 1
+    responses.append(line)
+    responses_current += 1
+
+def get_responses(line_separator='<br>'):
+    global responses, responses_current, serial_port
+    
+    if serial_port:
+        lines = serial_port.readlines()
+        for line in lines:
+            append_response("grbl: " + line)          
+    responce_text = ""
+    while responses:
+        responce_text += responses.popleft() + line_separator
+    responses.clear()
+    responses_current = 0
+    return responce_text
+    
+    
+
+
+def connect(port, baudrate):
+    global serial_port    
+    serial_port = serial.Serial(port, baudrate, timeout=0.1)
+
+def close():
+    global serial_port
+    if serial_port:
+        serial_port.close()
+        serial_port = None
+        return True
+    else:
+        return False
+                    
+def is_connected():
+    global serial_port
+    return bool(serial_port)
+
+
+
+def queue_for_sending(gcode):
+    global gcode_queue, total_items_queued_in_batch, gcode_queue_count
+    gcode = gcode.strip()
+    if gcode:
+        print "adding to queue: %s" % (gcode)
+        gcode_queue.append(gcode + '\n')
+        gcode_queue_count += 1
+        total_items_queued_in_batch += 1
+
+def is_queue_empty():
+    global gcode_queue
+    return not bool(gcode_queue)
+
+def clear_queue():
+    global gcode_queue, gcode_queue_count, total_items_queued_in_batch
+    gcode_queue.clear()
+    gcode_queue_count = 0
+    total_items_queued_in_batch = 0
+    
+def get_queue_percentage_done():
+    global total_items_queued_in_batch, gcode_queue_count
+    if total_items_queued_in_batch == 0: 
+        print get_responses('\n')
+        return ""
+    return str(100-100*gcode_queue_count/total_items_queued_in_batch)
+    
+def send_queue_as_ready():
+    """Continuously call this to keep processing queue."""
+    global serial_port, gcode_queue, grbl_buffer_current, GRBL_BUFFER_MAX
+    global total_items_queued_in_batch, gcode_queue_count
+    
+    if serial_port:
+        try:
+            lines = serial_port.readlines()
+            for line in lines:
+                append_response("grbl: " + line)
+                grbl_buffer_current -= 1
+    
+            if gcode_queue:
+                if grbl_buffer_current < GRBL_BUFFER_MAX:
+                    line = gcode_queue.popleft()
+                    gcode_queue_count -= 1
+                    print "sending to grbl: %s" % (line)
+                    serial_port.write(line)
+                    grbl_buffer_current += 1
+                    #append_response("sent to grbl: %s - buffer_current: %d" % (line,grbl_buffer_current))
+            else:
+                total_items_queued_in_batch = 0
+        except OSError:
+            # Serial port appears closed => reset
+            close()
+        except ValueError:
+            # Serial port appears closed => reset
+            close()            
+            
+
