a501bc965187fd898136301505b97a7420c34635
Stefan Hechenberger <stefan@nortd.com>
2011-08-01 13:03:33 +0200

    Added repetitive point deletion. Any point that is within DELETION_EPSILON distance from the last used point is ignored. This also has the effect of merging geometry made from short lines into segments.

--- write_gcode.py
+++ write_gcode.py
@@ -1,7 +1,16 @@
 
+import math
 
 X = 0
 Y = 1
+
+# repetitive point deletion
+###########################
+# Any point that is within this distance from the
+# last used point is ignored.
+# This also has the effect of merging geometry made from
+# short lines into one segment.
+DELETION_EPSILON_SQUARED = 0.01**2
 
 def write_GCODE(boundarys, feedrate, laser_intensity, scale=1.0, xoff=0.0, yoff=0.0):
     feed = 2000.0
@@ -11,22 +20,28 @@ def write_GCODE(boundarys, feedrate, laser_intensity, scale=1.0, xoff=0.0, yoff=
     glist.append("F%0.3f\n"%feedrate)
     glist.append("S%0.3f\n"%laser_intensity)
     nsegment = 0
+    x_prev = 0.0
+    y_prev = 0.0
     for layer in range((len(boundarys)-1),-1,-1):
         path = boundarys[layer]
         for segment in range(len(path)):
-            nsegment += 1
             vertex = 0
             x = path[segment][vertex][X]*scale + xoff
             y = path[segment][vertex][Y]*scale + yoff
-            glist.append("G00X%0.3f"%x+"Y%0.3f"%y+"\n")
+            if ((x_prev-x)**2 + (y_prev-y)**2) > DELETION_EPSILON_SQUARED:
+                glist.append("G00X%0.3f"%x+"Y%0.3f"%y+"\n")
+                nsegment += 1
+                x_prev,y_prev = x,y
             for vertex in range(1,len(path[segment])):
                 x = path[segment][vertex][X]*scale + xoff
                 y = path[segment][vertex][Y]*scale + yoff
-                glist.append("G01X%0.3f"%x+"Y%0.3f"%y+"\n")
+                if ((x_prev-x)**2 + (y_prev-y)**2) > DELETION_EPSILON_SQUARED:
+                    glist.append("G01X%0.3f"%x+"Y%0.3f"%y+"\n")
+                    x_prev,y_prev = x,y                
     glist.append("S0\n") # reset laser intensity
     glist.append("G00X0Y0F20000\n") # reset laser intensity
     glist.append("%\n")
-    print "wrote",nsegment,"G code toolpath segments"
+    print "wrote",nsegment-1,"G code toolpath segments"
     return ''.join(glist)
 
   
