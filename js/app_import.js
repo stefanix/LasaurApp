f636d8cdc6c6b3fb4e63cf1594dbbbc93e49489d
Stefan Hechenberger <stefan@nortd.com>
2011-09-21 02:39:51 -0400

    removed redundant import button

--- js/app_import.js
+++ js/app_import.js
@@ -1,25 +1,10 @@
 $(document).ready(function(){
   
-  $("#dpi_radio_set").buttonset();
-  $('#dpi_radio_72').click(function(e){
-    $('#dpi_value').val('72');
-  });
-  $('#dpi_radio_90').click(function(e){
-    $('#dpi_value').val('90');
-  });
-  $('#dpi_radio_other').click(function(e){
-    $('#dpi_radio_set').hide();
-    $('#dpi_value').show();
-  });
-  $('#dpi_radio_90').trigger('click').button("refresh");
-  
-
   // G-Code Canvas Preview
   var icanvas = new Canvas('#import_canvas');
   icanvas.background('ffffff'); 
   // file upload form
-  $('#svg_upload_button').button();
-  $('#svg_upload_button').click(function(e){
+  $('#svg_upload_file').change(function(e){
     var input = $('#svg_upload_file').get(0)
     if (typeof window.FileReader !== 'function') {
       $().uxmessage('error', "This requires a modern browser with File API support.");
@@ -56,6 +41,27 @@ $(document).ready(function(){
   });
 
 
+  // setting up dpi selector
+  $("#dpi_radio_set").buttonset();
+  $('#dpi_radio_72').click(function(e){
+    $('#dpi_value').val('72');
+    $('#svg_upload_file').trigger('change');
+  });
+  $('#dpi_radio_90').click(function(e){
+    $('#dpi_value').val('90');
+    $('#svg_upload_file').trigger('change');
+  });
+  $('#dpi_radio_other').click(function(e){
+    $('#dpi_radio_set').hide();
+    $('#dpi_value').show();
+  });
+  $('#dpi_value').blur(function(e){
+    $('#svg_upload_file').trigger('change');
+  });
+  $('#dpi_radio_90').trigger('click').button("refresh");
+
+
+  // setting up add to queue button
   $("#import_to_queue").button();  
   $("#import_to_queue").click(function(e) {
     var gcodedata = $('#import_results').text();
