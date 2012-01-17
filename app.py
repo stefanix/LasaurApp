68ce1f1852c1d6b4545e425adbe2e7053c83367a
Stefan Hechenberger <stefan@nortd.com>
2012-01-17 23:30:57 +0100

    Better ways to define the serial port: (1) first argument on command line (2) in a lasaurapp.conf file on the first line (3) by best guess on linux and osx, using first port starting with tty.usbmodem

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 
 import time
+import sys, os
 import os.path
 import wsgiref.simple_server
 from bottle import *
 import serial_manager, serial
 
 
-# ARDUINO_PORT = '/dev/tty.usbmodem621'
-# ARDUINO_PORT = '/dev/tty.usbmodemfa131'
-# ARDUINO_PORT = '/dev/tty.usbmodemfd122'
-ARDUINO_PORT = '/dev/tty.usbmodemfd121'
-BITSPERSECOND = 57600
+SERIAL_PORT = None
+BITSPERSECOND = 9600
+CONFIG_FILE = "lasaurapp.conf"
+GUESS_PPREFIX = "tty.usbmodem"
+
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -44,10 +45,10 @@ def hello_handler():
 
 @route('/connect')
 def connect_handler():
-    global ARDUINO_PORT, BITSPERSECOND
+    global SERIAL_PORT, BITSPERSECOND
     try:
-        serial_manager.connect(ARDUINO_PORT, BITSPERSECOND)
-        ret = "Serial connected to %s:%d." % (ARDUINO_PORT, BITSPERSECOND)  + '<br>'
+        serial_manager.connect(SERIAL_PORT, BITSPERSECOND)
+        ret = "Serial connected to %s:%d." % (SERIAL_PORT, BITSPERSECOND)  + '<br>'
         serial_manager.write("\r\n\r\n")
         time.sleep(1) # allow some time to receive a prompt/welcome
         serial_manager.flushInput()
@@ -92,9 +93,9 @@ def serial_handler(connect):
     if connect == '1' and not serial_manager.is_connected():
         print 'js is asking to connect serial'      
         try:
-            global arduino_port, baudrate
-            serial_manager.connect(ARDUINO_PORT, BITSPERSECOND)
-            ret = "Serial connected to %s:%d." % (ARDUINO_PORT, BITSPERSECOND)  + '<br>'
+            global SERIAL_PORT, BITSPERSECOND
+            serial_manager.connect(SERIAL_PORT, BITSPERSECOND)
+            ret = "Serial connected to %s:%d." % (SERIAL_PORT, BITSPERSECOND)  + '<br>'
             time.sleep(1.0) # allow some time to receive a prompt/welcome
             resp = serial_manager.get_responses('<br>')
             if resp == "": resp = ret
@@ -155,7 +156,49 @@ def queue_pct_done_handler():
 #         #     # this is necessary because inkscape stores everything in px units
 #         # return gcode
 #     return "You missed a field."
-    
 
-debug(True)
-run_with_callback(host='localhost')
+
+
+if len(sys.argv) == 2:
+    # (1) get the serial device from the argument list
+    SERIAL_PORT = sys.argv[1]
+    print "Using serial device '"+ SERIAL_PORT +"' from command line."
+else:    
+    if os.path.isfile(CONFIG_FILE):
+        # (2) get the serial device from the config file
+        fp = open(CONFIG_FILE)
+        line = fp.readline().strip()
+        if len(line) > 3:
+            SERIAL_PORT = line
+            print "Using serial device '"+ SERIAL_PORT +"' from '" + CONFIG_FILE + "'."
+            
+        
+
+if not SERIAL_PORT:
+    # (3) try best guess the serial device if on linux or osx
+    devices = os.listdir("/dev")
+    for device in devices:
+        if device[:len(GUESS_PPREFIX)] == GUESS_PPREFIX:
+            SERIAL_PORT = "/dev/" + device
+            print "Using serial device '"+ SERIAL_PORT +"' by best guess."
+            break
+    
+            
+
+if SERIAL_PORT:
+    debug(True)
+    run_with_callback(host='localhost')    
+else:         
+    print "-----------------------------------------------------------------------------"
+    print "ERROR: LasaurApp doesn't know what serial device to connect to!"
+    print "On Linux or OSX this is something like '/dev/tty.usbmodemfd121' and on"
+    print "Windows this is something like 'COM1', 'COM2', 'COM3', ..."
+    print "The serial port can be supplied in one of the following ways:"
+    print "(1) First argument on the  command line."
+    print "(2) In a config file named '" + CONFIG_FILE + "' (located in same directory)"
+    print "    with the serial port string on the first line."
+    print "(3) Best guess. On Linux and OSX the app can guess the serial name by"
+    print "    choosing the first device it finds starting with '"+ GUESS_PPREFIX +"'."
+    print "-----------------------------------------------------------------------------"
+
+
