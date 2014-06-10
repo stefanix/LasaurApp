52994012edfd8bb5e1d6db7801540f1cd1eced91
Stefan Hechenberger <stefan@nortd.com>
2014-06-10 11:21:07 +0200

    note to use doubles in config.h

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -36,13 +36,13 @@
 
 
 #ifndef V1401
-  #define CONFIG_X_STEPS_PER_MM 32.80839895 //microsteps/mm
-  #define CONFIG_Y_STEPS_PER_MM 32.80839895 //microsteps/mm
-  #define CONFIG_Z_STEPS_PER_MM 32.80839895 //microsteps/mm
+  #define CONFIG_X_STEPS_PER_MM 32.80839895 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+  #define CONFIG_Y_STEPS_PER_MM 32.80839895 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+  #define CONFIG_Z_STEPS_PER_MM 32.80839895 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
 #else
-  #define CONFIG_X_STEPS_PER_MM 88.88888888 //microsteps/mm
-  #define CONFIG_Y_STEPS_PER_MM 90.90909090 //microsteps/mm
-  #define CONFIG_Z_STEPS_PER_MM 33.33333333 //microsteps/mm
+  #define CONFIG_X_STEPS_PER_MM 88.88888888 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+  #define CONFIG_Y_STEPS_PER_MM 90.90909090 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
+  #define CONFIG_Z_STEPS_PER_MM 33.33333333 //microsteps/mm (no integers, e.g. use 80.0 instead of 80)
 #endif
 #define CONFIG_PULSE_MICROSECONDS 5
 #define CONFIG_FEEDRATE 8000.0 // in millimeters per minute
