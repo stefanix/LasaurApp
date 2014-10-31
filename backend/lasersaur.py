fc25c8ea287d6020640470c8b99e2ec77d2fcebe
Stefan Hechenberger <stefan@nortd.com>
2014-10-31 12:53:08 +0100

    serial timings

--- backend/lasersaur.py
+++ backend/lasersaur.py
@@ -275,6 +275,7 @@ class SerialLoopClass(threading.Thread):
                     else:
                         self.request_status = 1  # processing -> normal request
                     last_status_request = time.time()
+            time.sleep(0.01)
             if self.stop_processing:
                 break
 
